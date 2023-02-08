c8f3f3e9e47fb8bfa4f3fa49a56f0e74118f907a
quomatz <quomatz@outlook.com>
2023-02-08 02:10:46 +0100

    dev: day11 still ongoing

--- src/year2022/day11.rs
+++ src/year2022/day11.rs
@@ -26,7 +26,43 @@ mod Monkeys{
         right: Operand,
       //assingnee : Operand - always New   
     }
-    
+
+    mod Parsers{
+        use nom::{error::Error, character::complete::digit1, combinator::map};
+        use nom::complete::tag;
+        use nom::sequence::Tuple;
+        use super::{Statement, Operand, ArithmeticalOperation};
+
+        struct StatementParser;
+
+        #[allow(non_snake_case)]
+        fn parse_Operand(input : &str) -> nom::IResult<&str,Operand,Error<&str>> {
+            alt(map(tag("old"),|_| Operand::Old),map(digit1,|num_str| u32::from_str_radix(num_str,10)))(input)
+        }
+        #[allow(non_snake_case)]
+        fn parse_Operation(input : &str) -> nom::IResult<&str,Operand,Error<&str>> {
+            let operator = char(input);
+            match operator
+            {
+                '+' => ArithmeticalOperation::Add,
+                '-' => ArithmeticalOperation::Substract,
+                '*' => ArithmeticalOperation::Multiply,
+                '/' => ArithmeticalOperation::Divide,
+                _ => return Error {input:input, },
+            }
+                
+        }
+
+        #[allow(non_snake_case)]
+        fn parse_Statement( input: &str) -> nom::IResult<&str, Statement, Error<&str>> {
+            let new = tag("new =")(input);
+            let operand1 = parse_Operand(input)?;
+            let operation = parse_Operation(input)?;
+            let operand2 = parse_Operand(input)?;
+
+            Ok(Statement{operand1,operation,operand2});
+        }
+    }
 }
 
 
