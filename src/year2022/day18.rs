e319def4de0e46da0f058c7185b7abd1d37a17c5
quomatz <quomatz@outlook.com>
2023-05-02 02:03:02 +0200

    dev(18): bfs once again

--- src/year2022/day18.rs
+++ src/year2022/day18.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
 
 use crate::{day0::Day, utils::tuple_map::TupleMap, year2022::day18::cube_arena::parser::point};
@@ -11,36 +13,39 @@ impl Day<2022, 18, CubeSpace, u32> for Day18 {
         input.sum()
     }
 
+    fn solve2(input: CubeSpace) -> u32 {
+        todo!()
+    }
+
     fn parse(input: &str) -> CubeSpace {
-        let points = input.lines().map(point).collect::<Vec<Point>>();
+        let points = input.lines().map(point).collect::<HashSet<Point>>();
         let (max_x, max_y, max_z) = points
             .iter()
             .map(|&Point { x, y, z }| (x, y, z))
             .multiunzip()
             .tmap(|v: Vec<K>| v.into_iter().max().unwrap());
         let mut cubes = CubeSpace::new(max_x + 1, max_y + 1, max_z + 1);
-
-        for p in points {
-            let mut nonzero_neighs = 0;
+        for p in &points {
             for q in p.neighbours() {
-                if cubes.get(q).unwrap_or(0) != 0 {
-                    nonzero_neighs += 1;
-                    cubes.sub1(q);
+                    if cubes.get(q).is_ok() {
+                        cubes.sub1(q);
+                    }
                 }
-            }
 
-            cubes.set(p, 6 - nonzero_neighs);
         }
-
+        cubes.set_points(points);
         cubes
     }
 }
 
 
 mod cube_arena {
+    use std::collections::HashSet;
+
     /// Field of coordinates
     pub type K = usize;
-    #[derive(Clone, Copy, Debug)]
+    #[derive(Clone, Copy, Debug, PartialEq,Eq)]
+    #[derive(Hash)]
     pub struct Point {
         pub x: K,
         pub y: K,
@@ -105,15 +110,20 @@ mod cube_arena {
 
     pub struct CubeSpace {
         cubes: Vec<u8>,
+        points: HashSet<Point>,
         depth: K,
         height: K,
         width: K,
     }
 
+
+    const CUBE_SIDES : u8 = 6;
+
     impl CubeSpace {
         pub fn new(width: K, height: K, depth: K) -> CubeSpace {
             CubeSpace {
-                cubes: vec![0; width * height * depth],
+                cubes: vec![CUBE_SIDES; width * height * depth],
+                points : HashSet::new(),
                 width,
                 height,
                 depth,
@@ -166,12 +176,27 @@ mod cube_arena {
         }
 
         pub fn sum(&self) -> u32 {
-            self.cubes.iter().map(|&i| i as u32).sum()
+            self.points.iter().map(|&p| self.get(p).unwrap() as u32).sum()
+        }
+
+        pub fn add_point(&mut self, p: Point)  {
+            self.points.insert(p);
+        }
+
+        pub fn set_points(&mut self, pts : HashSet<Point>)
+        {
+            self.points = pts;
+        }
+
+        pub(crate) fn get_points(&self) -> &HashSet<Point> {
+            &self.points
         }
     }
 
     #[cfg(test)]
     mod tests {
+        use crate::year2022::day18::cube_arena::CUBE_SIDES;
+
         use super::{CubeSpace, Point};
 
         #[test]
@@ -194,16 +219,17 @@ mod cube_arena {
             let width = 5;
             let height = 3;
             let length = 7;
-            let init_val  = 6;
             let mut cubes = CubeSpace::new(width, height, length);
             for x in 0..width {
                 for y in 0..height {
                     for z in 0..length {
-                        cubes.set(Point { x, y, z }, init_val);
+                        let p = Point { x, y, z };
+                        cubes.set(p, CUBE_SIDES);
+                        cubes.add_point(p);
                     }
                 }
             }
-            assert_eq!(cubes.sum() as usize,height*width*length*(init_val as usize));
+            assert_eq!(cubes.sum() as usize,height*width*length*(CUBE_SIDES as usize));
         }
     }
 }
